dd45982082bacb9251cbd08462330363088e8158
sangwon0724 <leews0724@naver.com>
2021-06-16 16:58:59 +0900

    1.0 React + Firebase Setup

---
+++ jack_sparrow/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLJEpP3DhITa2jTxFnlRD8g4kdUaweiwE",
+    authDomain: "jacksparrow-96e1a.firebaseapp.com",
+    projectId: "jacksparrow-96e1a",
+    storageBucket: "jacksparrow-96e1a.appspot.com",
+    messagingSenderId: "474471845860",
+    appId: "1:474471845860:web:e364a6cf858071f74a01f1",
+    measurementId: "G-T4J1LVMRJT"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
