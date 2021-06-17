9a32ad451dbe53ceb515a7ecb10a928addfd94b2
sangwon0724 <leews0724@naver.com>
2021-06-17 16:59:25 +0900

    3.2 Getting the Nweets

--- jack_sparrow/src/routes/Home.js
+++ jack_sparrow/src/routes/Home.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "myDatabase";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+      const dbNweets = await dbService.collection("jack").get();
+      dbNweets.forEach((document) => {
+        const nweetObject = {
+          ...document.data(),
+          id: document.id,
+        };
+        setNweets((prev) => [nweetObject, ...prev]);
+      });
+    };
+    useEffect(() => {
+      getNweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("jack").add({
@@ -17,6 +31,7 @@ const Home = () => {
       } = event;
       setNweet(value);
     };
+    console.log("nweets : "+JSON.stringify(nweets));
     return (
       <div>
         <form onSubmit={onSubmit}>
@@ -29,6 +44,13 @@ const Home = () => {
           />
           <input type="submit" value="Nweet" />
         </form>
+        <div>
+            {nweets.map((nweet) => (
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+            ))}
+        </div>
       </div>
     );
 };
